a9652b9d08acdfb2ee6d608d9b5313ac9026cd8f
gumgu <bienlee@naver.com>
2023-10-05 13:57:48 +0900

    [refactor] axios import수정, 불필요한 코드 삭제

--- BoardSimple/frontend/src/api/FileAxios.js
+++ BoardSimple/frontend/src/api/FileAxios.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import store from "@/store";
+import axios from "./axios";
 
 const serverUrl = 'http://localhost:8080/'
 
--- BoardSimple/frontend/src/api/ReplyAxios.js
+++ BoardSimple/frontend/src/api/ReplyAxios.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axios";
 import router from "@/router";
 
 const serverUrl = 'http://localhost:8080/'
